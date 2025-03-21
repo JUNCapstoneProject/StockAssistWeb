53c6d90d00ddc4d4ed889d2c6ca961baf1b7caea
wildyoung <ondskan@naver.com>
2025-03-22 04:06:29 +0900

    이메일 인증 구현

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import MyPortfolioUnlink from './pages/MyPortfolioUnlink';
 import MyPortfolioLink from './pages/MyPortfolioLink';
 import IndividualStock from './pages/IndividualStock';
 import AiAnalysis from './pages/Aianalysis';
+import EmailVerification from './pages/EmailVerification'; // 새로 추가한 이메일 인증 컴포넌트
 import './App.css';
 
 function App() {
   return (
     <Router> 
-      <div className="App"> {/* test */}
+      <div className="App">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -18,6 +19,7 @@ function App() {
           <Route path="/portfolio/link" element={<MyPortfolioLink />} />
           <Route path="/stock/:symbol" element={<IndividualStock />} />
           <Route path="/ai-analysis" element={<AiAnalysis />} />
+          <Route path="/verify" element={<EmailVerification />} /> {/* 추가된 라우트 */}
         </Routes>
       </div>
     </Router>
