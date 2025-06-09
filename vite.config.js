4d8a3d924e895fdb7cab564f36d650edcc6922eb
wildyoung <ondskan@naver.com>
2025-06-09 15:11:45 +0900

    .

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      includeAssets: ['images/*.png'], // ✅ public/logo.png → logo.png
+      manifest: {
+        name: '투자인',
+        short_name: '투자인',
+        description: '투자인',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'Logo.png', // ✅ public/ 생략
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'Logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        display: 'standalone', // 선택 사항이지만 넣어주면 좋아요
+      },
+    }),
+  ],
   base: '/',
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',  // 너의 백엔드 서버 주소
+        target: 'http://localhost:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
