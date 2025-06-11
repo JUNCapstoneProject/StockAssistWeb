61ffd39689b626dec7ee2d5e0fe5a2244b9273f0
wildyoung <ondskan@naver.com>
2025-06-11 10:15:57 +0900

    .

--- src/components/modules/Report/ReportHeader.jsx
+++ src/components/modules/Report/ReportHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from '../../../redux/features/auth/authSelectors';
 
